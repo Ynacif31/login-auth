69432c6ca4328885893b553cd538e61b0e7210d1
Ygor Nacif <ygor.nacif00@gmail.com>
2025-03-20 13:46:20 -0300

    SecurityFilter implemented

---
+++ src/main/java/com/ygornacif/login_auth/infra/security/SecurityFilter.java
@@ -0,0 +1,61 @@
+package com.ygornacif.login_auth.infra.security;
+
+import com.ygornacif.login_auth.domain.entities.User;
+import com.ygornacif.login_auth.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Optional;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(SecurityFilter.class);
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            var token = recoverToken(request);
+            if (token != null) {
+                logger.debug("Token recuperado: {}", token);
+                var login = tokenService.validateToken(token);
+                if (login != null) {
+                    logger.debug("Login validado: {}", login);
+                    Optional<User> user = Optional.ofNullable(userRepository.findByEmail(login).orElseThrow(() -> new RuntimeException("Usuário não encontrado")));
+                        var authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+                        var authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Erro durante a autenticação", e);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) {
+            return null;
+        }
+        return authHeader.replace("Bearer ", "");
+    }
+}
